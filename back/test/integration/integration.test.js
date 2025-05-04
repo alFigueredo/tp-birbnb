13270774345852544ae4cd99febd8063d7a5c206
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2025-05-04 15:58:10 -0300

    Add: first integration test

---
+++ back/test/integration/integration.test.js
@@ -0,0 +1,11 @@
+import request from "supertest";
+import { app } from "../../index.js";
+
+describe("GET /health", () => {
+  test("Testeo de healthcheck", async () => {
+    const response = await request(app).get("/health");
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+    expect(response.body.status).toBe("ok");
+  });
+});
