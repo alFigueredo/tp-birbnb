88466fa3c8bc7e2b2d11d413e14b848ff059f102
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2025-07-01 21:32:38 -0300

    Add: enter para buscar reservas

--- front/app/reservas/page.js
+++ front/app/reservas/page.js
@@ -1,67 +1,62 @@
-'use client';
+"use client";
 
-import{useEffect, useState} from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function ReservasList() {
   const [reservas, setReservas] = useState([]);
-  const [searchId, setSearchId] = useState('');
+  const [searchId, setSearchId] = useState("");
   const [reservasFiltradas, setReservasFiltradas] = useState([]);
   const [busquedaRealizada, setBusquedaRealizada] = useState(false);
 
-
-    /*useEffect(() => {
+  /*useEffect(() => {
     axios
       .get("http://localhost:4000/reserva")
       .then((res) => setReservas(res.data || []))
       .catch((err) => console.error("Error al obtener reservas: ", err));
   }, []);*/
 
-
-  //EJEMPLOS DE RESERVAS 
+  //EJEMPLOS DE RESERVAS
   useEffect(() => {
-  setTimeout(() => {
-    setReservas([
-      {
-        _id: '1',
-        alojamiento: { nombre: 'Departamento Rio de Janeiro' },
-        cantHuespedes: 2,
-        rangoFechas: { fechaInicio: '2025-07-01', fechaFin: '2025-07-03' },
-        estado: 'CONFIRMADA',
-      },
-      {
-        _id: '1',
-        alojamiento: { nombre: 'Miravida SOHO' },
-        cantHuespedes: 2,
-        rangoFechas: { fechaInicio: '2025-08-01', fechaFin: '2025-08-20' },
-        estado: 'PENDIENTE',
-      },
-      {
-        _id: '2',
-        alojamiento: { nombre: 'Hilton Puerto Madero' },
-        cantHuespedes: 3,
-        rangoFechas: { fechaInicio: '2025-08-10', fechaFin: '2025-08-12' },
-        estado: 'PENDIENTE',
-      },
-      {
-        _id: '3',
-        alojamiento: { nombre: 'Ayres de Recoleta Plaza' },
-        cantHuespedes: 3,
-        rangoFechas: { fechaInicio: '2025-10-01', fechaFin: '2025-10-20' },
-        estado: 'CONFIRMADA',
-      }
-
-
-    ]);
-  }, 1000);
-}, [])
+    setTimeout(() => {
+      setReservas([
+        {
+          _id: "1",
+          alojamiento: { nombre: "Departamento Rio de Janeiro" },
+          cantHuespedes: 2,
+          rangoFechas: { fechaInicio: "2025-07-01", fechaFin: "2025-07-03" },
+          estado: "CONFIRMADA",
+        },
+        {
+          _id: "1",
+          alojamiento: { nombre: "Miravida SOHO" },
+          cantHuespedes: 2,
+          rangoFechas: { fechaInicio: "2025-08-01", fechaFin: "2025-08-20" },
+          estado: "PENDIENTE",
+        },
+        {
+          _id: "2",
+          alojamiento: { nombre: "Hilton Puerto Madero" },
+          cantHuespedes: 3,
+          rangoFechas: { fechaInicio: "2025-08-10", fechaFin: "2025-08-12" },
+          estado: "PENDIENTE",
+        },
+        {
+          _id: "3",
+          alojamiento: { nombre: "Ayres de Recoleta Plaza" },
+          cantHuespedes: 3,
+          rangoFechas: { fechaInicio: "2025-10-01", fechaFin: "2025-10-20" },
+          estado: "CONFIRMADA",
+        },
+      ]);
+    }, 1000);
+  }, []);
 
   const buscarReserva = () => {
     const encontradas = reservas.filter((r) => r._id === searchId);
     setReservasFiltradas(encontradas);
-    setBusquedaRealizada(true);                                   
+    setBusquedaRealizada(true);
   };
-
 
   return (
     <div className="min-h-screen bg-neutral-200 pt-21 pb-10 px-4">
@@ -69,13 +64,16 @@ export default function ReservasList() {
         📅 Reservas 📅
       </h1>
 
-       {/* BARRA DE BUSQUEDA */}
+      {/* BARRA DE BUSQUEDA */}
       <div className="font-bold flex justify-center items-center mb-8 gap-2">
         <input
           type="text"
           placeholder="Buscar reserva por ID de usuario"
           value={searchId}
           onChange={(e) => setSearchId(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") buscarReserva();
+          }}
           className="border border-black rounded px-4 py-2 w-64 text-sm"
         />
         <button
@@ -85,8 +83,8 @@ export default function ReservasList() {
           Buscar
         </button>
       </div>
-      
-       {/* USUARIO SIN RESERVAS*/}
+
+      {/* USUARIO SIN RESERVAS*/}
       {busquedaRealizada && reservasFiltradas.length === 0 && (
         <p className="text-center text-gray-600 mb-8">
           Este ID no posee reservas❌.
@@ -103,9 +101,7 @@ export default function ReservasList() {
             <p className="text-lg text-gray-800">
               Alojamiento: {reserva.alojamiento.nombre}
             </p>
-            <p className="text-lg text-gray-800">
-              Estado: {reserva.estado}
-            </p>
+            <p className="text-lg text-gray-800">Estado: {reserva.estado}</p>
             <p className="text-sm text-gray-600">
               Desde: {reserva.rangoFechas.fechaInicio}
             </p>
@@ -118,3 +114,4 @@ export default function ReservasList() {
     </div>
   );
 }
+
