0ffecac5798fb1dc92288bbfdb042f91bf8f70dd
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2025-07-09 18:52:30 -0300

    Add: alojamiento title is now a link

--- front/app/components/Alojamientos/AlojamientoDatos.js
+++ front/app/components/Alojamientos/AlojamientoDatos.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
+
 export default function AlojamientoDatos({ aloj }) {
   return (
     <div className="space-y-3 text-left">
-      
-     <h2 className="text-3xl font-bold text-blue-800">{aloj.nombre}</h2>
+      <h2 className="text-3xl font-bold text-blue-800 hover:text-blue-950">
+        <Link href={`/alojamientos/${aloj._id}`}>{aloj.nombre}</Link>
+      </h2>
 
-     <p className="text-gray-800">{aloj.descripcion}</p>
+      <p className="text-gray-800">{aloj.descripcion}</p>
 
       <p className="text-green-600 font-bold text-lg">
         💸${aloj.precioPorNoche.toLocaleString()} por noche
