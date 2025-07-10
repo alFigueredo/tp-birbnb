a317b7408dd64621f8b220244e0fc9c5d77ea7cc
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2025-07-10 20:54:21 -0300

    Fix: cypress default timeout

--- front/cypress.config.js
+++ front/cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+    defaultCommandTimeout: 7000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
